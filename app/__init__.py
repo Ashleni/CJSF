7b7cdc95dbca3372b2d6fde4077547b9d42d691e
Ashleni <sroy2005@outlook.com>
2022-12-07 12:42:22 -0500

    position stack api working

---
+++ app/__init__.py
@@ -0,0 +1,46 @@
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+import requests           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+
+
+
+
+#position stack!
+
+def latitude(location):
+    key = open("keys/key_positionstack.txt", "r").read()
+    key = key.strip()
+    query = location
+    link = 'http://api.positionstack.com/v1/forward?access_key=' + key + '&query=' + query
+    #region = '&region=Rio+de+Janeiro&limit=1'
+    #print(link)
+    r = requests.get(link) 
+    info = r.json()
+    return info['data'][0]['latitude']
+    
+    
+def longitude(location):
+    key = open("keys/key_positionstack.txt", "r").read()
+    key = key.strip()
+    query = location
+    link = 'http://api.positionstack.com/v1/forward?access_key=' + key + '&query=' + query
+    #region = '&region=Rio+de+Janeiro&limit=1'
+    #print(link)
+    r = requests.get(link) 
+    info = r.json()
+    return info['data'][0]['longitude']
+    
+
+    
+print(longitude("new york city"))
+print(latitude("new york city"))
+
+
+#http://api.positionstack.com/v1/forward?access_key=5e990e9a2a02b3952708f993eaeb5c44&query=1600 Pennsylvania Ave NW, Washington DC
