8d3944d3c77ab814cc75caf8ca4f5aabc739c605
Kevin Wang <69407438+kev1n@users.noreply.github.com>
2023-02-16 17:52:57 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -238,6 +238,6 @@ def distance(lat1, lon1, lat2, lon2):
     a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
     return 12742 * asin(sqrt(a)) #2*R*asin...
 
-if __name__ == "__main__": #false if this file imported as module
+def app(): #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.run()
